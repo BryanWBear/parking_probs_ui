a017dd118697f12135e4ca6206e9d8c195d4f454
tweiss <tweiss@ucsb.edu>
2023-05-03 21:18:43 -0700

    change box stuff

--- ControlPanel.jsx
+++ ControlPanel.jsx
@@ -13,39 +13,40 @@ function ControlPanel(props) {
   });
 
   const { weekday, onWeekdayChange, startTime, onStartTimeChange, offset, onOffsetChange} = props;
-
+  
   const handleWeekdayChange = (event) => {
     onWeekdayChange(event.target.value);
-  };
+  };  
+
 
   const marks = [
     {
       value: 15,
-      label: '15 min',
+      label: '15',
     },
     {
       value: 30,
-      label: '30 min',
+      label: '30',
     },
     {
       value: 45,
-      label: '45 min',
+      label: '45',
     },
     {
       value: 60,
-      label: '60 min',
+      label: '60',
     },
     {
       value: 75,
-      label: '75 min',
+      label: '75',
     },
     {
       value: 90,
-      label: '90 min',
+      label: '90',
     },   
     {
       value: 105,
-      label: '105 min',
+      label: '105',
     },
   ];
 
@@ -61,51 +62,104 @@ function ControlPanel(props) {
     return () => window.removeEventListener('resize', handleResize);
   }, []);
 
+  
+
   return (
     <div style={{
       position: 'absolute',
-      top: '10px',
-      right: '10px',
-      width: `calc(10vw + ${windowDimensions.width * 0.15}px)`,
-      height: `calc(10vh + ${windowDimensions.height * 0.2}px)`,
-      backgroundColor: '#ffffff',
+      top: '5px',
+      right: '5px',
+      width: `calc(10vw + ${windowDimensions.width * 0.2}px)`,
+      height: `calc(10vh + ${windowDimensions.height * 0.15}px)`,
+      backgroundColor: 'gainsboro',
       border: '1px solid #000000',
+      // display: "flex",
+      padding: 20,
+      // overflow: "hidden",
+      //alignItems: "center",
+      //justifyContent: "center",
     }}>
-      <InputLabel id="start-time-picker">Start Time</InputLabel>
-      <TimePicker
-        id="start-time-picker"
-        value={startTime}
-        onChange={onStartTimeChange}
-        minutesStep={15}
-      />
-      <InputLabel id="duration-picker">Parking Duration</InputLabel>
-      <Slider
-        id='duration-picker'
-        aria-label="Parking Duration"
-        value={offset}
-        onChange={onOffsetChange}
-        valueLabelDisplay="auto"
-        step={15}
-        marks={marks}
-        min={15}
-        max={105}
-      />
-      <InputLabel id="demo-simple-select-label">Weekday</InputLabel>
-      <Select
-      labelId="demo-simple-select-label"
-      id="demo-simple-select"
-      value={weekday}
-      label="Weekday"
-      onChange={handleWeekdayChange}
-    >
-      <MenuItem value={"Monday"}>Monday</MenuItem>
-      <MenuItem value={"Tuesday"}>Tuesday</MenuItem>
-      <MenuItem value={"Wednesday"}>Wednesday</MenuItem>
-      <MenuItem value={"Thursday"}>Thursday</MenuItem>
-      <MenuItem value={"Friday"}>Friday</MenuItem>
-      <MenuItem value={"Saturday"}>Saturday</MenuItem>
-      <MenuItem value={"Sunday"}>Sunday</MenuItem>
-    </Select>
+      
+     <div style={{
+      display: "flex",
+      //alignItems: "center",
+      //justifyContent: "center",
+     }}>
+
+        <div style={{
+          marginRight: "10px",
+          //alignItems: "center",
+          //justifyContent: "center",
+        }}>
+
+          <InputLabel sx={{
+              color: 'black',
+              fontWeight: 'bold',
+            }}
+            id="start-time-picker">Start Time</InputLabel>
+            <TimePicker
+              id="start-time-picker"
+              value={startTime}
+              onChange={onStartTimeChange}
+              minutesStep={15}
+            />
+        </div>
+        <div style={{
+          //alignItems: "center",
+          //justifyContent: "center",
+        }}>
+            <InputLabel sx={{
+              color: 'black',
+              fontWeight: 'bold',
+            }} id="demo-simple-select-label">Weekday</InputLabel>
+            <Select
+            labelId="demo-simple-select-label"
+            id="demo-simple-select"
+            value={weekday}
+            label="Weekday"
+            onChange={handleWeekdayChange}
+          >
+            <MenuItem value={"Monday"}>Monday</MenuItem>
+            <MenuItem value={"Tuesday"}>Tuesday</MenuItem>
+            <MenuItem value={"Wednesday"}>Wednesday</MenuItem>
+            <MenuItem value={"Thursday"}>Thursday</MenuItem>
+            <MenuItem value={"Friday"}>Friday</MenuItem>
+            <MenuItem value={"Saturday"}>Saturday</MenuItem>
+            <MenuItem value={"Sunday"}>Sunday</MenuItem>
+            </Select>
+        </div>
+
+      </div>
+
+      <div style={{
+        marginTop: "30px",
+        //marginLeft: "30px",
+        //marginRight: "30px",
+        alignItems: "center",
+        justifyContent: "center",
+      }}>
+
+          <InputLabel sx={{
+              color: 'black',
+              fontWeight: 'bold',
+            }}
+            id="duration-picker">Parking Duration (in minutes)</InputLabel>
+          <Slider
+            id='duration-picker'
+            aria-label="Parking Duration"
+            value={offset}
+            onChange={onOffsetChange}
+            valueLabelDisplay="auto"
+            step={15}
+            marks={marks}
+            min={15}
+            max={105}
+            sx={{
+              width: 350,
+              color: 'royalblue',
+            }}
+          />
+      </div>
     </div>
   );
 }
